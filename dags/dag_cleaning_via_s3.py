60562c23f5dfe2166b82f945d85e787fa7bdddd5
Tetiana Lysenko <tatyana.a.lysenko@gmail.com>
2023-02-23 12:29:00 +0100

    refactored to OOP

--- dags/dag_cleaning_via_s3.py
+++ dags/dag_cleaning_via_s3.py
@@ -1,9 +1,3 @@
-'''
-1. Get the file from S3
-2. Transform the data
-3. Put the file back to S3
-'''
-
 import pandas as pd
 from datetime import datetime, timedelta
 from airflow import DAG
@@ -12,69 +6,65 @@ from airflow.operators.python_operator import PythonOperator
 from io import StringIO
 import yaml
 
-# Get config
+'''
+1. Get the file from S3
+2. Transform the file
+3. Put the file back to S3
+'''
 
-with open('./config.yaml') as cfg:
-    config = yaml.safe_load(cfg)
 
-AWS_CONN_ID = config['S3']['conn_id']
-S3_BUCKET_NAME = config['S3']['bucket_name']
-FILE_NAME = config['S3']['file_name']
+class EtlJob:
 
-### Helper functions
+    def __init__(self):
+        self.SRC_FILE_NAME = None
+        self.TGT_FILE_NAME = None
+        self.AWS_CONN_ID = None
+        self.S3_BUCKET_NAME = None
 
-def get_file_from_s3(file_name=FILE_NAME,
-                     s3_bucket_name=S3_BUCKET_NAME,
-                     aws_conn_id=AWS_CONN_ID):
+    def get_config(self):
+        with open('./config.yaml') as cfg:
+            config = yaml.safe_load(cfg)
 
-    s3_hook = S3Hook(aws_conn_id)
-    print('file name', file_name)
-    s3_object = s3_hook.get_key(file_name, s3_bucket_name)
+        self.AWS_CONN_ID = config['S3']['conn_id']
+        self.S3_BUCKET_NAME = config['S3']['bucket_name']
+        self.SRC_FILE_NAME = config['S3']['file_name']
+        self.TGT_FILE_NAME = 'processed.csv'
 
-    csv_string = s3_object.get()['Body'].read().decode('utf-8')
+        return self.AWS_CONN_ID, self.S3_BUCKET_NAME, self.SRC_FILE_NAME, self.TGT_FILE_NAME
 
-    df = pd.read_csv(StringIO(csv_string))
-    return df
+    def get_file_from_s3(self):
 
-def put_file_to_s3(df,
-                   file_name=FILE_NAME,
-                   aws_bucket_name=S3_BUCKET_NAME,
-                   aws_conn_id=AWS_CONN_ID):
+        s3_hook = S3Hook(self.AWS_CONN_ID)
+        print('file name', self.SRC_FILE_NAME)
+        s3_object = s3_hook.get_key(self.SRC_FILE_NAME, self.S3_BUCKET_NAME)
 
-    s3_hook = S3Hook(aws_conn_id)
+        csv_string = s3_object.get()['Body'].read().decode('utf-8')
 
-    csv_string = StringIO(df.to_csv(index=False))
+        df = pd.read_csv(StringIO(csv_string))
+        return df
 
-    s3_hook.load_string(
-            string_data=csv_string.getvalue(),
-            bucket_name=aws_bucket_name,
-            replace=True,
-            key='processed.csv'
-        )
-    return True
+    def put_file_to_s3(self, df):
+        s3_hook = S3Hook(self.AWS_CONN_ID)
+        csv_string = StringIO(df.to_csv(index=False))
+        s3_hook.load_string(
+                string_data=csv_string.getvalue(),
+                bucket_name=self.S3_BUCKET_NAME,
+                replace=True,
+                key=self.TGT_FILE_NAME
+            )
+        return True
 
-def transform(df):
-    df.dropna(inplace=True)
-    return df
+    @staticmethod
+    def transform(df):
+        df.dropna(inplace=True)
+        return df
 
-### ETL function
+    def run_etl(self):
+        self.get_config()
+        df = self.get_file_from_s3()
+        df = self.transform(df)
+        self.put_file_to_s3(df)
 
-def ETL(file_name=FILE_NAME,
-        s3_bucket_name=S3_BUCKET_NAME,
-        aws_conn_id=AWS_CONN_ID):
-
-    df = get_file_from_s3(file_name,
-                     s3_bucket_name=s3_bucket_name,
-                     aws_conn_id=aws_conn_id)
-
-    df = transform(df)
-
-    put_file_to_s3(df,
-                   file_name='transformed.csv',
-                   aws_bucket_name=s3_bucket_name,
-                   aws_conn_id=aws_conn_id)
-
-### DAG
 
 default_args = {
     'owner': 'airflow',
@@ -85,13 +75,14 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id='csv_from_s3_to_s3_v01',
+    dag_id='csv_from_s3_to_s3_v03',
     default_args=default_args,
     description='Download CSV file from S3, clean, and write to S3',
-    schedule_interval=None,
-    tags=['cleaning']
+    schedule_interval=None
 )
 
+etl_job = EtlJob()
+
 task_etl = PythonOperator(task_id='ETL',
-                                 python_callable=ETL,
-                                 dag=dag)
+                          python_callable=etl_job.run_etl,
+                          dag=dag)
